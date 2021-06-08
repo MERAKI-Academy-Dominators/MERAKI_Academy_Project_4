821d7ff5f44cc6db2ea7234b72482ee7938f2080
RuaaAlsugheir <alsugheirruaa@gmail.com>
2021-06-09 00:07:15 +0300

    why_give_blood_pages

---
+++ frontend/src/components/why give blood page/how_blood_is_used.js
@@ -0,0 +1,60 @@
+import React from "react";
+
+export default function HowBloodIsUsedPage() {
+  return (
+    <div>
+      <div id="">
+        <h2>How blood is used</h2>
+        <p>
+          Blood or the components of blood are used to treat patients with
+          medical conditions such as anemia, cancer blood disorders, and those
+          having surgery.
+        </p>
+        <h3>Blood components</h3>
+        <p>
+          Blood is made up of a number of components, including{" "}
+          <span style={{ fontWeight: "bolder" }}>red blood cells</span>,{" "}
+          <span style={{ fontWeight: "bolder" }}>platelets</span> and{" "}
+          <span style={{ fontWeight: "bolder" }}>plasma</span>. Each of these
+          can be used to treat many different conditions. Blood is usually
+          separated into its individual components or parts, so a patient can be
+          given the particular component they need. This makes the most of every
+          blood donation, as the components in one unit of blood (or one
+          donation) can be used to treat different patients.
+        </p>
+        <h3>Blood transfusions</h3>
+        <p>
+          Donated blood or components are given to a patient in a blood
+          transfusion. Blood transfusions are given via an intravenous line into
+          a blood vessel. Around two thirds of the blood donated is used to
+          treat medical conditions including anaemia, cancer and blood disorders
+          Nearly a third is used in surgery and emergencies including childbirth
+          Usage varies between hospitals depending on their particular
+          specialties. We work closely with hospitals to make sure valuable
+          blood donations are used appropriately. The demand for blood from
+          hospitals has fallen due to increased efficiency, but new donors are
+          always needed to make sure there is enough blood to treat those who
+          need it.
+        </p>
+        <h3>Blood transfusions for people who are dying</h3>
+        <p>
+          Blood can be vital for people with medical conditions or who are
+          having surgery. But blood transfusions can also improve the quality of
+          life for people whose illness has no cure. Karen Clarke, a community
+          nurse who gives transfusions to terminally ill people in their own
+          homes, says: 'These vital transfusions give patients a better quality
+          of life and the energy and ability to enjoy this precious, final time
+          with their families'.
+        </p>
+        <h3>Blood not used for transfusion</h3>
+        <p>
+          Sometimes, blood may not be suitable for transfusion. This blood can
+          still benefit patients in areas like the development of treatments and
+          therapies. Blood can be very useful for research and training, and to
+          help develop tests. We do not sell blood, or its components, for
+          non-clinical use.
+        </p>
+      </div>
+    </div>
+  );
+}
