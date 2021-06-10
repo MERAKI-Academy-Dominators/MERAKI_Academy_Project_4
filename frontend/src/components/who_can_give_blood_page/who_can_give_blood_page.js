246bdeb3a16cd93cc1ba31b3bad5998699fa14a8
RuaaAlsugheir <alsugheirruaa@gmail.com>
2021-06-10 22:34:07 +0300

    criteria for blood donation page with design

--- frontend/src/components/who_can_give_blood_page/who_can_give_blood_page.js
+++ frontend/src/components/who_can_give_blood_page/who_can_give_blood_page.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "./who_can_give_blood_style.css";
 
 export default function WhenCanIBloodPage() {
   return (
-    <>
+    <div>
       <h1>When can I give blood?</h1>
       <p>
         Sometimes it is not possible to give blood, or we may ask you to wait
@@ -11,39 +12,48 @@ export default function WhenCanIBloodPage() {
       </p>
       <br />
       <h2>Frequently asked</h2>
-      <h3>Can I give blood again if I have give blood before?</h3>
+      <h3>
+        Can I give blood again if I have <span>given blood recently</span>?
+      </h3>
       <p>
         If you have given blood before, you will have to wait for at least 8
         weeks before you donate again.
       </p>
       <h3>
-        Can I donate blood if I have been diagnosed with AIDS, syphilis,
-        Hepatitis B or C ?
+        Can I donate blood if I have been diagnosed with <span>AIDS</span>,{" "}
+        <span>syphilis</span>, <span>Hepatitis B</span> or <span>C</span> ?
       </h3>
       <p>
         You will be permanently deferred from blood donation if you have been
-        diagnose with any of the diseases mentioned above.
+        diagnosed with any of the diseases mentioned above.
       </p>
-      <h3>Can I give blood after receiving coronavirus vaccine?</h3>
+      <h3>
+        Can I give blood after receiving coronavirus <span>vaccine?</span>
+      </h3>
       <p>
-        As a precaution, you need to wait 7-days after receiving each COVID-19
+        As a precaution, you need to wait 7 days after receiving each COVID-19
         vaccine before donating blood, plasma, or platelets. This wait time
         applies to all COVID-19 vaccinations, regardless of which vaccine you
         receive.
       </p>
-      <h3>Can I donate if I have a cold?</h3>
+      <h3>
+        Can I donate if I have a <span>cold?</span>
+      </h3>
       <p>
         No, if you are sneezing and coughing or very congested you should not
         attend. It is important that you do not have any infection at the time
         of donating. If you are unsure it is best not to give blood.
       </p>
-      <h3>Can I donate if I feel ill or have a cold sore?</h3>
+      <h3>
+        Can I donate if I feel ill or have a <span>cold sore</span>?
+      </h3>
       <p>
         If you are feeling under the weather it’s best that you wait until you
         feel better before you give blood.
       </p>
       <h3>
-        Can I donate blood if I am taking antibiotics or have an infection?
+        Can I donate blood if I am <span>taking antibiotics</span> or have an{" "}
+        <span>infection</span>?
       </h3>
       <p>
         You must be completely healed or recovered from any infection for at
@@ -51,8 +61,8 @@ export default function WhenCanIBloodPage() {
         may need to wait a period of time after your last tablet.
       </p>
       <h3>
-        Can I donate if I am pregnant, have recently been pregnant or breast
-        feeding my baby?
+        Can I donate if I am <span>currently pregnant</span>, have{" "}
+        <span>given birth</span> or <span>breast feeding</span> my baby?
       </h3>
       <p>
         You are not able to give blood from time of pregnancy to 6 months after
@@ -61,8 +71,8 @@ export default function WhenCanIBloodPage() {
         the first 6 months have passed.
       </p>
       <h3>
-        Can I give blood if I am receiving medical treatment or taking
-        medication?
+        Can I give blood if I am receiving medical treatment or{" "}
+        <span>taking medication</span>?
       </h3>
       <p>
         We’ll need to check that you can donate. Whether or not you can donate
@@ -70,44 +80,59 @@ export default function WhenCanIBloodPage() {
         for which you are being treated.
       </p>
       <h3>
-        Can I give blood if I have been to the dentist or received dental
-        treatment?
+        Can I give blood if I have been to the dentist or{" "}
+        <span>received dental treatment</span>?
       </h3>
       <p>
         Simple fillings and check ups are usually ok after 24 hours. However,
         some treatments will mean a longer deferral.
       </p>
-      <h3>Can I give blood if I have a heart condition?</h3>
+      <h3>
+        Can I give blood if I have a <span>heart condition</span>?
+      </h3>
       <p>
-        You will permanently deferred from blood donation if you have a heart
+        You will be permanently deferred from blood donation if you have a heart
         condition.
       </p>
-      <h3>Can I donate if I have got a tattoo or a body piercing?</h3>
-      If you had this done recently you may have to wait 1 year before you can
-      donate again.
-      <h3>Can I give blood if I have had cancer?</h3>
+      <h3>
+        Can I donate if I have got a <span>tattoo</span> or a{" "}
+        <span>body piercing</span>?
+      </h3>
       <p>
-        All types of cancer are permanently deferred with the exception of
-        successfully treated cervical cancer and skin cancer.
+        If you had this done recently you may have to wait 1 year before you can
+        donate again.
       </p>
       <h3>
-        Can I give blood if I have received blood products or tissue transplant?
+        Can I give blood if I have had <span>cancer</span>?
+      </h3>
+      <p>
+        All types of cancer are permanently deferred with the exception of
+        successfully treated cervical cancer and skin cancer (basal and
+        squamous).
+      </p>
+      <h3>
+        Can I give blood if I have received <span>blood products</span> or{" "}
+        <span>tissue transplant</span>?
       </h3>
       <p>
         If you have received blood products or tissue transplant, you will have
         to wait for 1 year before you can donate blood again.
       </p>
-      <h3>Can I donate blood if I have high blood pressure?</h3>
+      <h3>
+        Can I donate blood if I have <span>high blood pressure</span>?
+      </h3>
       <p>
         You can donate as long as your blood pressure is below 180 systolic
         (first number) and below 100 diastolic (second number) at the time of
         donation.
       </p>
-      <h3>Can I give blood if I have had acupuncture?</h3>
+      <h3>
+        Can I give blood if I have had <span>acupuncture</span>?
+      </h3>
       <p>
         If you have had acupuncture, you will have to wait for 1 year before you
         can donate blood again.
       </p>
-    </>
+    </div>
   );
 }
