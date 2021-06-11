7d94150d64884f3a778dde14d3b1af85ccf220bb
AhmadAlAraj06 <81652458+AhmadAlAraj06@users.noreply.github.com>
2021-06-11 20:54:36 +0300

    why give blood finished

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import Demand from "./components/auth/why-give-blood/Demand";
+import Type from "./components/auth/why-give-blood/Type";
+import How from "./components/auth/why-give-blood/How";
+
 
 const App = () => {
-	return <div className="App">App component</div>;
+	return <div>   
+	<Route exact path = "/Demand" component={Demand}/>
+	<Route exact path = "/Type" component={Type}/>
+	<Route exact path = "/How" component={How}/>
+
+	</div>
 };
 
 export default App;
---
+++ frontend/src/components/auth/why-give-blood/Demand.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import demand from './Demand.css'
+
+
+const Demand = () => {
+	return ( <div className = "font">
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <h2>Demand for different blood types</h2>
+        <h5>We maintain a regular supply of all blood types to ensure the right blood is available to everyone who <br/> needs it. We need enough of the right types of blood to meet day-to-day patient needs and to cope with <br/> emergencies. </h5>
+        <br/>
+        <br/>
+        <h2>Which blood types are in demand?</h2>
+        <h5>Some blood types are in higher demand from hospitals than others.</h5>
+        <h5>For example, it's estimated that around 8% of people have O negative blood, but O negative makes up <br/> 13% of requests from hospitals.</h5>
+        <h5>This is because anyone can receive the red cells from O negative donors. So O negative red cells can <br/> be used in emergencies or when a person's blood type in unknown.</h5>
+        <h5>Sometimes we need to target specific blood types to increase stock levels. That's why we sometimes <br/> contact regular donors with the particular blood type we need, and ask them to give blood.</h5>
+        <br/>
+        <br/>
+        <h2>Which blood types are rare?</h2>
+        <h5>A rare blood type is any blood type that is needed by patients and is difficult to find in the population.</h5>
+        <br/>
+        <br/>
+        <h2>Why is closely-matched blood important?</h2>
+        <h5>Some patients, particularly those who require on-going transfusions, need blood that closely matches <br/> their blood type.</h5>
+        <h5>This involves matching blood more extensively than the main blood types.</h5>
+        <h5>Some rare subtypes are more common in specific communities, which is why we particularly need more <br/> blood donors from black, Asian and minority ethnic communities.</h5>
+        <br/>
+        <br/>
+        <h2>How long does blood last after it’s been donated?</h2>
+        <h5>red blood cells can be stored for up to 35 days<br/>platelets can be stored for up to 7 days<br/>plasma can be stored for up to 3 years</h5>
+        <h5>We only collect what we need to avoid wasting any donations.</h5>
+        
+    </div>
+    )
+};
+
+
+export default Demand ;
