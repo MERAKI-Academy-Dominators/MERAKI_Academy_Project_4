2cd9f6d9fc9a8ffcd7210efbd8b5b559910ae5e8
AhmadAlAraj06 <81652458+AhmadAlAraj06@users.noreply.github.com>
2021-06-11 23:18:21 +0300

    why give blood (font family) finished

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import Demand from "./components/auth/why-give-blood/Demand";
+import How from "./components/auth/why-give-blood/How";
+import Type from "./components/auth/why-give-blood/Type";
 
 const App = () => {
-	return <div className="App">App component</div>;
+	return <div>
+	<Route exact path = "/Demand" component={Demand}/>
+	<Route exact path = "/How" component={How}/>
+	<Route exact path = "/Type" component={Type}/>
+
+	</div>
 };
 
 export default App;
