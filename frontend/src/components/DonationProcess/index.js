add5a10b4c9a3021c1d802663d98b7ed7f54f317
RuaaAlsugheir <alsugheirruaa@gmail.com>
2021-06-12 03:00:17 +0300

    styling for two info parts is done

--- frontend/src/components/DonationProcess/index.js
+++ frontend/src/components/DonationProcess/index.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React from "react";
+import { Route } from "react-router-dom";
+import BeforeBloodDonationPage from "./before_blood_donation";
+import DuringBloodDonationPage from "./during_blood_donation";
+import AfterBloodDonationPage from "./after_blood_donation";
 
-const Header = () => {
-	return <div className="App">Header component</div>;
-};
-
-export default Header;
+export default function DonationProcess() {
+  return (
+    <div>
+      <br />
+      <h1>Donation process</h1>
+      <br />
+      <Route
+        exact
+        path="/DonationProcess/BeforeBloodDonation"
+        component={BeforeBloodDonationPage}
+      />
+      <Route
+        exact
+        path="/DonationProcess/DuringBloodDonation"
+        component={DuringBloodDonationPage}
+      />
+      <Route
+        exact
+        path="/DonationProcess/AfterBloodDonation"
+        component={AfterBloodDonationPage}
+      />
+    </div>
+  );
+}
